e8aec43ede50c37cf41eb8e91fd0fbec2874fc6c
Syed, Javedhussain <jasyed@visa.com>
2022-03-05 18:56:51 +0000

    Services

---
+++ Services/UK_Process/UK_Visa_Process.sh
@@ -0,0 +1,422 @@
+Mail Visa Details to Michelle in detail
+Michelle will discuss with Crystal regarding an opening on UK Office
+
+
+1. Student Level should be from Level 7. It ranges from 3-9 (Amount 7,14,337.19 Indian Rupee), 28 Days old must. Need in Spouse Account? Consent Letter ?
+2. Duration should be more than 12 months
+3. Children less than 18 years are eligible for UK Visa
+
+Maintenance:
+1. Need to show funds on Student VISA
+2. Need to show funds on Dependent VISA
+3. Need to Pay IHS --> Medical Insurance, once student visa expires we need to pay IHS
+4. Embassy_Fee
+5. Visa Fee 35K, 35-45 days will take. If we pay more 25K then will get dependent visa in 5 days
+6. Does IELTS is required ?
+7. Passport, Financial Bank Statements, 28 days,TB Test.
+
+
+
+Visa Fees 348 Pounds, IHS Charges,
+After Landing UK, From Post Office Resident Permit Card need to collect with in 10 days.
+
+Works as a PartTime for 20 hours a week.
+
+Tier2 SponsorShip, minimum salary Pounds 30000 minimum
+
+70 Points,
+English Test
+Job Offer
+Birth Certificate ?
+
+
+
+15 Lakhs - University Fee
+Embassy Fee
+IHS -
+TB Test
+
+10,000 Pounds - 13.5 Lakhs
+5000 Pounds ->
+Application Fee, IHS,
+
+8.5 Lakh -->
+
+22,000 Pounds
+Spouse Visa -
+Immigration Health Surcharge 470 Pounds per year
+
+
+east london e13 0lr to shelton square paddington
+
+FS Number, NI Number(National Insurance number)
+
+Linkedin and Indeed
+
+PSW - Post Study Work for 2 Years, will be used for 10 Years ILR, Long Residence.
+UK BI Immigration Provides PSW
+We can work FullTime on PSW.
+
+birmingham
+
+
+
+New System UKVI from Oct 5th no Tier4,
+Point Based Immigration - 50 Points
+PSW - 2 Years
+
+
+
+
+BRP Card -  Biometric Residence Permit. After landing UK purchase a SIM.
+NHS Number - National Health Service
+
+
+
+Tier2 - Sponsored Worker Visa
+
+
+
+
+
+
+--
+
+FLAT NO 302, LILY BLOSSOMS APARTMENT, UPPERPPALY
+ATTAPUR, UPPERPALLY, RAJENDRANAGAR, HYDERABAD
+500030
+
+--
+UK International Recharge
+
+https://relocation.graebel.com/
+jasyed@visa.com
+Syed@786_
+
+--
+CMM - HYDERABAD
+
+ADDRESS
+CENTRE FOR MIGRATION MEDICINE
+PLOT NO: 89, MLA COLONY,
+ROAD NO 12, BANJARA HILLS,
+HYDERABAD-500034
+PHONE : +91 40 2360 7770
+MOBILE : +91 8500777000
+EMAIL : CMM@MIGRATIONMEDICINE.COM
+
+
+Syed:
+UNIQUE CMM ID
+9NXY8VX3YO
+
+Mehar CMM ID:
+U9TEL1EKBE
+
+Asad CMM ID:
+WI7A0QV0JF
+
+
+SYED JAVEDHUSSAIN, FLAT NO. 302, LILY BLOSSOMS APARTMENT,
+UPPERPALLY, RAJENDRANAGAR, ATTAPUR, BESIDE SDR PEARL PALACE, PILLAR NO 176, HYDERABAD, TELANGANA, 500030.
+
+
+
+Your form has been submitted
+Your submission reference is
+G8P5-3K0A-X2QY
+
+
+https://mymobility.pwc.com
+jasyed@visa.com
+Syed_786@
+
+
+QADEER ABDUL, 34 CECIL ROAD,
+PLAISTOW
+LONDON
+E13 0LR
+
+
+2-5-19/1/P UPPERPALLY,
+RAJENDRANAGAR,
+K.v. Rangareddy
+Telangana,
+500030
+
+
+2-5-19/1/P, Flat No. 302, Lily Blossoms Apartment
+Beside SDR Pearl Palace Function Hall
+Pillar No 176, Upperpally, Rajendranagar, K.v. Rangareddy
+Hyderabad
+Telangana
+500030
+India
+
+
+VISA EUROPE
+Paddington Central
+1 Sheldon Square, London W2 6TT, United Kingdom
++44 20 7795 5777
+
+
+SYED JAVEDHUSSAIN
+T6939329
+
+MEHARUNNISAN BEGUM
+U7342785
+
+SYED ASAD HUSSAIN
+T6939830
+
+
+
+
+The Chronicle
+4 Norwich Street, London, EC4A 1DR
+
+
+
+
+
+************* Pre-Travel Work *************
+3.  International Driving License = 9700869944 - Abdul Mubin => 3000/- - Done
+4.  Transcripts Collection OU Syed -- Applied - Done
+6.  Migration Collection OU Syed -- Applied - Done
+6.  Declarations Collection OU Syed -- Done
+7.  Transcripts Degree KU Mehar - Applied - NA
+8.  Transcripts Master KU Mehar - Applied - NA
+10. Medium Certificate Masters KU Mehar - Pending
+11. Convocation Certificate Degree KU Mehar - Applied - Done
+12. Medium Certificate Degree KU Syed - Done
+14. Need to share latest PF Passbook with Sunil (Reflecting Jan Month tax) --  Done
+15. Fill in your passenger locator form - With in 48 Hours of flights
+https://www.gov.uk/provide-journey-contact-details-before-travel-uk  -- Done
+15.1. Take Covid Test with in 72 hours of flights
+15.2. Book 2 Corona test before we travel -- Its Mandatory - Done
+16. Online Webcheckin - Done
+17. Aarogya Sethu App - Done
+18. Covid test on Monday 5th April 2021 - Done
+19. Take COS Letter Print - Done
+
+
+Shopping:
+2. Face Masks - 5
+3. Jeans - 2 - Done
+5. Perfumes, Deo - 2 - Done
+6. Belt - 3 - Done
+10. Medicines - Done
+15. One Year Sims validity Mehar - Done
+16. Sim International Roaming - Done
+
+*******************************************
+1. Book Covid TestKit for all 3 - Done
+2. Webcheckin Indigo, Health Check Declaration --> 31st March -- Done
+3. Cargo - 2nd April -- Done
+4. Covid Test - 215 PILLAR --> 5th April - Done
+5. Passenger Locator Form - 48 hours before --> 6th April - Done
+6. Webcheckin BA 24 Hours Before --> 7th April - Done
+*******************************************'
+
+
+
+
+Folder:
+Syed:
+1. Passport Original - Done
+2. Aadhar Card Original - Done
+5. Visa Letter Xerox Copy - Done
+6. Resume - Done
+7. Offer Letter - Done
+8. PF Documents - Done
+9. Id Card - Done
+10. TB Test Report Original - Done
+14. COS Letter Print - Done
+3. Flight Itinerary Prints - Done
+13. Address Page - Done
+15. Hotel Booking - Done
+16. Covid Test Kit Receipt and Invoice - Done
+11. Covid Test Report Print - Done
+
+12. Passenger Locator Form - Done
+4.  WebCheckIn Documents - Done
+
+
+Mehar:
+1. Passport Original - Done
+2. Aadhar Card Original - Done
+4. TB Test Report Original - Done
+7. Marriage Certificate Xerox - Done
+9. Visa Letter Copy - Done
+8. Address Page - Done
+3. Flight Itinerary Prints - Done
+5. Covid Test Report Print - Done
+
+6. Passenger Locator Form - Done
+
+
+Asad:
+1. Passport Original - Done
+2. Aadhar Card Original - Done
+5. Date Of Birth Certificate Xerox - Done
+6. Visa Letter Copy - Done
+3. TB Test Report Original - Done
+4. Covid Test Report - Done
+
+
+
+*******************************************
+
+Tom's Mail:
+1. To ask about who is Visa HR for RTW ? -- Nancy Boulos
+2. Letter for Bank Account - Done
+
+*******************************************
+
+The Chronicle Aparthotel
+4 Norwich Street London EC4A 1DR
+https://www.silverdoorapartments.com/the-chronicle-aparthotel-london-13960/
+apartment type:      2 bedroom 2 bathroom
+apartment size:       Approx 80 sqm
+notes:           On-site restaurant*, On-site fitness centre*
+cancellation policy:    14 day
+
+*******************************************
+Overseas Citizenship of India , earlier its a PA Card
+*******************************************
+The Chronicle Aparthotel
+4 Norwich Street Holborn,
+London EC4A 1DR,
+United Kingdom.
+
+*******************<Covid19 - 2 day 8day Test Kit>************************
+Expense Code for ICICI Credit Card - Ask Expense Code to Sajjad once I get a Bill from Concur during April 14-18 2021.
+Once we get a Bill for 630 Pounds in ICICI like it will reflect in Concur, at that time write a mail to
+Sajjad adding invoice and ask for the Org Code and add that code in the "My Expenses" as ask Ghana to approve it.
+Hope at that time Sajjad should be my manager so that he will be the approval.
+*******************************************
+
+Barclays:
+
+Your membership number is
+204013980444
+Account type	Personal only membership
+Registered	09 April 2021
+Ways to log in	Passcode log-in only
+
+https://www.barclays.co.uk/logindetails
+Membership No:204013980444
+Passcode: 68999 --> 07866
+Memorable Word: 07860786
+SortCode: 20-55-59
+Account No: 33310434
+
+Mobile Banking Memorable Code:
+078666
+
+BARCGB210YE - SWIFT Code Details
+Bank: Barclays Bank Plc
+Country: United Kingdom
+City: Leicester
+SWIFT Code:
+BARCGB210YE
+BARCGB210YE
+Branch: BARCLAYS BUSINESSDIRECT
+Address:
+- Post / ZIP Code: LE87 2BB
+
+
+BARCLAYS BANK PLC - BARCLAYS BUSINESSDIRECT is located at LEICESTER city in United Kingdom and the bank branch's address - - Post / ZIP Code: LE87 2BB
+
+The SWIFT Code is BARCGB210YE. Branch code is the last three characters of the SWIFT Code - 0YE
+
+Barclays IBAN example in the United Kingdom	GB29NWBK60161331926819
+
+4605
+App Pwd: 07866
+*******************************************
+NHS: 7253974409 --> Javed
+NHS: 7253994876 --> ASAD
+NHS: 7253958675 --> MEHAR
+*******************************************
+Syed:  +44-7498969898 -->
+Mehar: +44-7498969897
+*******************************************
+EE Password: 0786
+
+*******************************************
+
+***********************<Closed>********************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+***************Post-Travel Work************
+Sim Card
+Transportation - Buses
+''
+0. Inform Tom about arrival --> Done
+0.1 Mail to Nancy Boulos attaching Visa Stamp and updating about joining date and quarantine period for collection of BRP, regarding Laptop. --> Done
+0.2 Declaration of Income Tax in EDP Portal, cut off date is 16th April, discussion with HR --> Done
+0.3 Sim Cards --> Done
+6. Bank Account (Required Passport, BRP, Address Proof(Enrollment/Appointment Letter)) - 1 DAY PROCESS, 5-6 days will receive pin --> Done
+5. Enrollment Letter with HR
+14. Mail to Siobhan with the UK Number for opening the accoung. Debit card and pin details and net banking details will be posted to The Chronicle Hotel. --> Done
+4. Collection of BRP (Carry Passport) --> Done
+7. NI Number --> Done
+8. GP (General Practice) Registration = Treatment Free --> Done
+11. Getting GP Number - Mandatory --> Done
+12. Getting NI Number - Mandatory --> Done
+12. Getting NHS Number - Mandatory --> Done
+13. Update test results on the 2nd day and 8th day need to upload into NHS portal --> Done
+16. Share NI number with Elisa and update in workday --> Done
+17. Apply for NHS - GP online as per Nancy Mail once we receive BRP. --> Done
+18. Share BRP with Nancy, Elisa and Carishma --> Done
+Flow: BRP --> Backside BRP we need to see NI Number exists --> NHS --> GP --> Done
+Bank Account --> Done
+NI Number --> Done
+19. Mail to Siobhan to update the latest UK number --> Done
+1. National Express - N/A
+2. Mega Bus - N/A
+3. Easy Bus - N/A
+9. Voter Registration (https://www.gov.uk/register-to-vote) --> In Progress - N/A
+10. Rail & Bus Card (Oyster, Rail Card) - Done
+21. Update Dependent Info on Workday once we receive BRP Card and Phone Number. --> Done
+22. Once we get SmartBenefits credentials add  dependants for Medical Allowances during enrolment window. --> Done
+23. Update Phone Number on Visa Insite Portal --> Done
+24. PF Meeting Schedule with Richa --> Done
+25. Barclays Mobile Banking app setup --> Done
+26. SAP Concur Remove Expense --> Done
+'
+*******************************************
+15. Visa Corporate Card Request
+20. Get an update from Sunil regarding PF Closure
+27. Mehar Bank Account
+28. Send documents TAX Returns to HMRC
+
+
+
+
+
+*******************************************
+https://cobalt2.balglobal.com/
+Username: jasyed@visa.com
+Password: Hadoop@786 --> Nation_786@
