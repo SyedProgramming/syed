a4a834edb986223b0ea2c44e0859dafef794e779
Syed, Javedhussain <jasyed@visa.com>
2022-03-05 16:58:26 +0000

    Services

---
+++ Services/HardDisk/Freelancing/freelancing-notes.sh
@@ -0,0 +1,205 @@
+****************************** Accounts ******************************
+Fiverr: https://www.fiverr.com/
+Username: syedacademy786
+Password: Hadoop@786
+mailid: info.syedacademy@gmail.com
+
+-------------------------------------
+peopleperhour.com
+Username: info.syedacademy@gmail.com
+Password: Hadoop@786
+
+
+Hi All,
+
+I'm Syed working as a Senior BigData Developer in a reputed organisation. I have a 8 years of experience in BigData Stack, if you have any requirement related to BigData, Datamarts, Datalake creations please let me know.
+
+I can also help you on Live Streaming Data as well as Batch execution's. It's totally up-to the requirement how we can design it.
+Programming Languages I'm expert in Java, Scala, Python, Go Lang.
+
+I'm strong in Data Structures & Algorithm and Design Patterns which helps me to provide a solution in an efficient manner.
+
+Thanks - Syed.
+
+
+-------------------------------------
+https://www.upwork.com/
+Account should to be active - contact support
+
+info.syedacademy@gmail.com
+Hadoop@786 - password
+-------------------------------------
+https://www.freelancer.com/
+info.syedacademy@gmail.com
+Hadoop@786 - password
+SyedAcademy - Username
+
+-------------------------------------
+https://www.guru.com/
+Syed Academy
+info.syedacademy@gmail.com
+Hadoop@786
+
+-------------------------------------
+https://www.toptal.com/
+info.syedacademy@gmail.com
+Hadoop@786
+Syed Academy
+
+Schedulled an Interview call at Dec 23rd 10 AM
+-------------------------------------
+https://www.truelancer.com/
+info.syedacademy@gmail.com
+Hadoop@786
+Syed Academy
+-------------------------------------
+https://zoom.us/
+info.syedacademy@gmail.com
+Hadoop@786
+
+-------------------------------------
+
+-------------------------------------
+
+-------------------------------------
+
+-------------------------------------
+Parttime jobs:
+Total Jobs
+Reed.co.uk
+Flex Jobs
+Nexxt
+WWR-We work remotely
+Authentic Jobs
+Task Rabbit
+Thumbstacj
+Contently
+WorkingNomads
+https://aquent.co.uk/
+
+
+-------------------------------------
+Social Media:
+Facebook
+Twitter
+Instagram
+Linkedin
+Youtube
+Tumblr
+Whatsapp
+Tiktok
+Quora
+Telegram
+Meetup
+Flickr
+Discord
+Medium
+
+www.syedacademy.com
+info.syedacademy@gmail.com
+
+Ideas:
+Udemy Free course
+Godaddy website
+Github Repo
+Docker Repo
+Linkedin Account - already existing(NASA one), just change email id from mail. to info.
+
+
+-------------------------------------
+
+
+-------------------------------------
+
+
+
+-------------------------------------'
+***************************************************************************************************************************************************************************************************************************************************
+
+Fiverr Gig:
+Contact me to get my “Free Advices” related to your project before placing any order.
+
+
+
+In this Gig I will provide all kinds of support related to big data projects and will try my best to help you out with any technical expertise.
+
+
+
+Just ping me your requirement, I will ensure that it will be done in an efficient and in optimised manner before the recommended deadline.
+
+
+
+Technologies used:
+
+Big Data
+Hadoop
+Spark, pySpark
+Kafka
+Java
+Python
+Scala
+Map-reduce
+HDFS
+Oozie
+SQL
+Hive
+Maven
+Impala
+Tableau
+Excel
+Pig
+Spark streaming
+Kudu
+Python
+Cassandra
+Hbase
+Storm
+Flume
+Airflow
+Docker
+Kubernetes
+Jenkins
+ANT
+Log4j
+JUnit
+HttpUnit
+Apache Tomcat
+Apache Weblogic
+Recursion
+Data Structures
+Algorithms
+Multithreading
+UML
+Client server Socket
+Graphical User Interface GUI
+
+
+Tools Used:
+
+IntelliJ IDEA
+Eclipse
+DataGrip
+DBVisualizer
+Postman
+SecureCRT
+Putty
+Cloudera
+Hortonworks
+Atom
+Code
+AWS
+Azure
+GCP
+IBM RAD
+IBM Websphere
+
+
+List doesn't end here I have Theoretical & Practical Knowledge in Computer Science Domain.
+
+
+
+Feel free to contact for any any kind of queries.
+
+100% refund if work not done as per requirement.
+***************************************************************************************************************************************************************************************************************************************************
+--------------------------------------------------------------------------------
